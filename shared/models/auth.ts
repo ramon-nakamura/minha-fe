367cb3132ad2b7515d8f3fc216bbd21b10dcea11
ramon-nakamura <51243645+ramon-nakamura@users.noreply.github.com>
2026-03-12 20:52:44 -0300

    Update auth.ts

--- shared/models/auth.ts
+++ shared/models/auth.ts
@@ -16,7 +16,8 @@ export const sessions = pgTable(
 export const users = pgTable("users", {
   id: varchar("id").primaryKey().default(sql`gen_random_uuid()`),
   email: varchar("email").unique().notNull(),
-  password: text("password").notNull(),
+  password: text("password"),
+  googleId: varchar("google_id").unique(),
   firstName: varchar("first_name"),
   lastName: varchar("last_name"),
   profileImageUrl: text("profile_image_url"),
